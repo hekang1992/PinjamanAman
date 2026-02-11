ea7bc40fa4d03ddd04722220df3a212348c2223f
hekang1992 <hekang19921111@hotmail.com>
2026-02-11 15:00:46 +0800

    two-page

--- PinjamanAman/Home/TwoViewCell.swift
+++ PinjamanAman/Home/TwoViewCell.swift
@@ -10,15 +10,15 @@ import SnapKit
 import FSPagerView
 
 class TwoViewCell: UITableViewCell {
-    
+
     var tapBlock: ((forgivenessModel) -> Void)?
-    
+
     var modelArray: [forgivenessModel]? {
         didSet {
             pagerView.reloadData()
         }
     }
-    
+
     private lazy var bgView: UIView = {
         let view = UIView()
         view.layer.cornerRadius = 20
@@ -26,25 +26,25 @@ class TwoViewCell: UITableViewCell {
         view.backgroundColor = UIColor(hexString: "#2D8847")
         return view
     }()
-    
+
     private lazy var ringImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "h_ring_a_image")
         return imageView
     }()
-    
+
     lazy var nameLabel: UILabel = {
-        let nameLabel = UILabel()
-        nameLabel.textAlignment = .center
-        nameLabel.text = "Memeriksa"
-        nameLabel.textColor = UIColor.init(hexString: "#267B3F")
-        nameLabel.font = UIFont.systemFont(ofSize: 12, weight: .regular)
-        nameLabel.backgroundColor = UIColor.white
-        nameLabel.layer.cornerRadius = 13
-        nameLabel.layer.masksToBounds = true
-        return nameLabel
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Memeriksa"
+        label.textColor = UIColor(hexString: "#267B3F")
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        label.backgroundColor = .white
+        label.layer.cornerRadius = 13
+        label.layer.masksToBounds = true
+        return label
     }()
-    
+
     private lazy var pagerView: FSPagerView = {
         let pagerView = FSPagerView()
         pagerView.dataSource = self
@@ -58,48 +58,51 @@ class TwoViewCell: UITableViewCell {
         pagerView.layer.borderWidth = 0
         return pagerView
     }()
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupUI()
         setupConstraints()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        pagerView.scrollToItem(at: 0, animated: false)
+    }
+
     private func setupUI() {
         backgroundColor = .clear
         selectionStyle = .none
-        
+
         contentView.addSubview(bgView)
         bgView.addSubview(ringImageView)
         bgView.addSubview(nameLabel)
         bgView.addSubview(pagerView)
     }
-    
+
     private func setupConstraints() {
+
         bgView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.centerX.equalToSuperview()
-            make.left.equalToSuperview()
+            make.edges.equalToSuperview()
             make.height.equalTo(86.pix())
-            make.bottom.equalToSuperview()
         }
-        
+
         ringImageView.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
             make.left.equalToSuperview().offset(5)
             make.width.height.equalTo(30)
         }
-        
+
         nameLabel.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
             make.right.equalToSuperview().offset(-12)
             make.size.equalTo(CGSize(width: 80, height: 26))
         }
-        
+
         pagerView.snp.makeConstraints { make in
             make.left.equalTo(ringImageView.snp.right).offset(8)
             make.centerY.equalToSuperview()
@@ -110,44 +113,33 @@ class TwoViewCell: UITableViewCell {
 }
 
 extension TwoViewCell: FSPagerViewDelegate, FSPagerViewDataSource {
-    
+
     func numberOfItems(in pagerView: FSPagerView) -> Int {
         return modelArray?.count ?? 0
     }
-    
+
     func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+
         let cell = pagerView.dequeueReusableCell(
             withReuseIdentifier: "CustomPagerCell",
             at: index
         ) as! CustomPagerCell
-        
+
         if let model = modelArray?[index] {
             cell.titleLabel.text = model.reason ?? ""
         }
-        self.cellPara(with: cell)
+
         return cell
     }
-    
+
     func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
         guard let model = modelArray?[index] else { return }
         tapBlock?(model)
     }
-    
-    private func cellPara(with cell: CustomPagerCell) {
-        cell.contentView.layer.shadowColor = UIColor.clear.cgColor
-        cell.contentView.layer.shadowRadius = 0
-        cell.contentView.layer.shadowOpacity = 0
-        cell.contentView.layer.shadowOffset = .zero
-        
-        cell.contentView.transform = CGAffineTransform.identity
-        
-        cell.contentView.backgroundColor = .clear
-        cell.backgroundColor = .clear
-    }
 }
 
 class CustomPagerCell: FSPagerViewCell {
-    
+
     lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = UIColor(hexString: "#FFFFFF")
@@ -156,34 +148,39 @@ class CustomPagerCell: FSPagerViewCell {
         label.textAlignment = .left
         return label
     }()
-    
+
     private lazy var containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .clear
         return view
     }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = nil
+    }
+
     private func setupUI() {
+
+        contentView.backgroundColor = .clear
+        backgroundColor = .clear
+
         contentView.addSubview(containerView)
         containerView.addSubview(titleLabel)
-        
-        setupConstraints()
-    }
-    
-    private func setupConstraints() {
+
         containerView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
+
         titleLabel.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
